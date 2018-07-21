34e563c16daf49797faa3ab797196b259091582c
ksb <ksb@users.sourceforge.net>
2018-07-21 19:55:07 +0200

    Added comment parser

---
+++ TIAEKtool/CommentParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIAEKtool
+{
+    public static class CommentParser
+    {
+        public delegate void AnnotationHandler(string type, string data);
+        static public void Parse(string comment, AnnotationHandler handler)
+        {
+            int len = comment.Length;
+            int pos = 0;
+            while (true)
+            {
+
+                pos = comment.IndexOf("@{", pos);
+                if (pos == -1) break;
+                pos += 2;
+                int start = pos;
+                while (pos < len)
+                {
+                    char c = comment[pos];
+                    if (!Char.IsLetterOrDigit(c) && c != '_') break;
+                    pos++;
+                }
+                if (pos == len) break;
+                string type = comment.Substring(start, pos - start);
+                start = pos;
+                pos = comment.IndexOf('}', pos);
+                if (pos == -1) break;
+                string data = comment.Substring(start, pos - start);
+                pos++;
+                handler(type, data);
+            }
+        }
+    }
+}
---
+++ TIAEKtoolTest/CommentParserTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TIAEKtoolTest
+{
+    [TestClass]
+    public class CommentParserTest
+    {
+        class ParseCtxt
+        {
+            public string foo;
+            public string bar;
+            public string foo_bar3;
+
+            public void Handle(string type, string data)
+            {
+                if (type == "foo") foo = data;
+                else if (type == "bar") bar = data;
+                else if (type == "foo_bar3") foo_bar3 = data;
+            }
+        }
+        [TestMethod]
+        public void TestParser()
+        {
+            ParseCtxt ctxt = new ParseCtxt();
+            TIAEKtool.CommentParser.Parse(" @{foo:kls}jlkd@{foo_bar3} @{bar sjkjk}", ctxt.Handle);
+            Assert.AreEqual(ctxt.foo, ":kls");
+            Assert.AreEqual(ctxt.bar, " sjkjk");
+            Assert.AreEqual(ctxt.foo_bar3, "");
+        }
+    }
+}
